7a844d5278a4c6b558eb2f135ceb85c22b9f7c70
Sylent-Sys <renaldiapriyanto419@gmail.com>
2024-05-07 23:32:41 +0700

    chore: Update import paths for pages and components

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,6 +1,6 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import ExploreContainer from '@components/ExploreContainer';
+import '@pages/Tab1.css';
 
 const Tab1: React.FC = () => {
   return (
