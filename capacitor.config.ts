ad05a40dbdb9fe5415effe7599bdefb3df110aab
Sylent-Sys <renaldiapriyanto419@gmail.com>
2024-05-08 19:36:24 +0700

    chore: Update SplashScreen plugin configuration

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,16 @@
+/// <reference types="@capacitor/splash-screen" />
+
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.sylentsys.akita',
   appName: 'akita',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    SplashScreen: {
+      backgroundColor: "#FCF4EE"
+    }
+  }
 };
 
 export default config;
