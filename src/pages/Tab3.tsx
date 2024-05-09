0e31991ebf011e9e175d608b897b495bf10edc92
Sylent-Sys <renaldiapriyanto419@gmail.com>
2024-05-09 18:29:04 +0700

    chore: Remove unused CSS imports in Tab1.tsx, Tab2.tsx, and Tab3.tsx

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,6 +1,5 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '@components/ExploreContainer';
-import '@pages/Tab3.css';
 
 const Tab3: React.FC = () => {
   return (
