bf03d1374688583dc4d12e3c87f07cc739b2938e
Sylent-Sys <renaldiapriyanto419@gmail.com>
2024-05-10 00:38:52 +0700

    refactor: Improve SkipText component in SplashScreen

--- src/components/SplashScreen/SkipText.tsx
+++ src/components/SplashScreen/SkipText.tsx
@@ -1,9 +1,12 @@
 import { useSwiper } from "swiper/react";
 
-function SkipText() {
+function SkipText({ invisible = false }) {
   const swiper = useSwiper();
   return (
-    <p className="skip-text" onClick={() => swiper.slideTo(3, 1000, false)}>
+    <p
+      className={`${invisible ? "hidden-text" : ""} skip-text`}
+      onClick={() => swiper.slideTo(3, 1000, false)}
+    >
       Skip
     </p>
   );
