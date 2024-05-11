c3bde5665a4cd7c84cbd308c29d3d2d105da75a3
Avriella <avriellasofianti.jj88@gmail.com>
2024-05-11 16:14:25 +0700

    feat: change 1

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonHeader, IonItem, IonLabel, IonList, IonListHeader, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
@@ -7,16 +7,30 @@ const Tab1: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
+          <h3 style={{ fontWeight: 'bold' }}>Hi,</h3>
+          <h3 style={{ fontWeight: 'bold', color: '#F3B660' }}>Dave Thio</h3>
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <IonRow>
+          <p style={{ fontWeight: 'bold', marginLeft: '19vw', color: '#774C2A' }}>Utama</p>
+          <p style={{ fontWeight: 'bold', marginLeft: '29vw', color: '#774C2A' }}>Analisa</p>
+
+
+        </IonRow>
+        {/* <ExploreContainer name="Tab 1 page" /> */}
+        {/* <IonCard>
+          <IonCardHeader>
+            <IonCardTitle>Card Title</IonCardTitle>
+            <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+          </IonCardHeader>
+
+          <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
+        </IonCard> */}
       </IonContent>
     </IonPage>
   );
