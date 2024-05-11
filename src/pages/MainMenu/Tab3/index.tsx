6a8f8c240d30dd70a3e625d841ae23aafd5ee4ff
Renaldi A.K <renaldiapriyanto419@gmail.com>
2024-05-11 11:45:36 +0700

    Refactor SplashScreen and MainMenu components (#2)
    * refactor: Update SplashScreen with SkipText component and routerLink for Siap! button
    
    * refactor: remove unused css import
    
    * refactor: Remove unused CSS imports and ExploreContainer component
    
    * refactor: Update imports in MainMenu component

--- src/pages/MainMenu/Tab3/index.tsx
+++ src/pages/MainMenu/Tab3/index.tsx
@@ -1,5 +1,4 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '@components/ExploreContainer';
 
 const Tab3: React.FC = () => {
   return (
@@ -15,7 +14,6 @@ const Tab3: React.FC = () => {
             <IonTitle size="large">Tab 3</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
       </IonContent>
     </IonPage>
   );
