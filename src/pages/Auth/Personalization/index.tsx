9741ee7510cfb681706f15815d6585363f257d2e
Sylent-Sys <renaldiapriyanto419@gmail.com>
2024-05-12 11:45:16 +0700

    feat: Add Swiper component for image carousel in AuthPersonalization page

--- src/pages/Auth/Personalization/index.tsx
+++ src/pages/Auth/Personalization/index.tsx
@@ -1,10 +1,23 @@
 import { IonContent, IonPage } from "@ionic/react";
-
+import "swiper/css/bundle";
+import "@ionic/react/css/ionic-swiper.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "@resources/scss/AuthPersonalization.scss";
 function AuthPersonalization() {
   return (
     <IonPage>
-      <IonContent fullscreen={true}>
-        <h1>Personalization</h1>
+      <IonContent
+        fullscreen={true}
+        className="ion-padding auth-personalization"
+      >
+        <Swiper>
+          <SwiperSlide></SwiperSlide>
+          <SwiperSlide></SwiperSlide>
+          <SwiperSlide></SwiperSlide>
+          <SwiperSlide></SwiperSlide>
+          <SwiperSlide></SwiperSlide>
+          <SwiperSlide></SwiperSlide>
+        </Swiper>
       </IonContent>
     </IonPage>
   );
