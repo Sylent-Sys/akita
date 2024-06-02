93d13092fed075a5af8b8542ff5ba559be861913
Sylent-Sys <renaldiapriyanto419@gmail.com>
2024-06-02 16:49:23 +0700

    chore: Add Tab4 component to MainMenu page

--- src/pages/MainMenu/index.tsx
+++ src/pages/MainMenu/index.tsx
@@ -6,11 +6,12 @@ import {
   IonTabButton,
   IonTabs,
 } from "@ionic/react";
-import { triangle, ellipse, square } from "ionicons/icons";
+import { home, book, cash, personCircle } from "ionicons/icons";
 import { Redirect, Route } from "react-router-dom";
 import Tab1 from "@pages/MainMenu/Tab1";
 import Tab2 from "@pages/MainMenu/Tab2";
 import Tab3 from "@pages/MainMenu/Tab3";
+import Tab4 from "@pages/MainMenu/Tab4";
 
 function MainMenu() {
   return (
@@ -19,20 +20,25 @@ function MainMenu() {
         <Route path="/mainmenu/tab1" render={() => <Tab1 />} />
         <Route path="/mainmenu/tab2" render={() => <Tab2 />} />
         <Route path="/mainmenu/tab3" render={() => <Tab3 />} />
+        <Route path="/mainmenu/tab4" render={() => <Tab4 />} />
         <Redirect exact path="/mainmenu" to="/mainmenu/tab1" />
       </IonRouterOutlet>
       <IonTabBar slot="bottom">
         <IonTabButton tab="tab1" href="/mainmenu/tab1">
-          <IonIcon aria-hidden="true" icon={triangle} />
-          <IonLabel>Tab 1</IonLabel>
+          <IonIcon aria-hidden="true" icon={home} />
+          <IonLabel>Dashboard</IonLabel>
         </IonTabButton>
         <IonTabButton tab="tab2" href="/mainmenu/tab2">
-          <IonIcon aria-hidden="true" icon={ellipse} />
-          <IonLabel>Tab 2</IonLabel>
+          <IonIcon aria-hidden="true" icon={book} />
+          <IonLabel>Resep</IonLabel>
         </IonTabButton>
         <IonTabButton tab="tab3" href="/mainmenu/tab3">
-          <IonIcon aria-hidden="true" icon={square} />
-          <IonLabel>Tab 3</IonLabel>
+          <IonIcon aria-hidden="true" icon={cash} />
+          <IonLabel>Keuangan</IonLabel>
+        </IonTabButton>
+        <IonTabButton tab="tab4" href="/mainmenu/tab4">
+          <IonIcon aria-hidden="true" icon={personCircle} />
+          <IonLabel>Profil</IonLabel>
         </IonTabButton>
       </IonTabBar>
     </IonTabs>
