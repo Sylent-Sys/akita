679731d2b4cf21afef35714361def99521f3ae8e
Sylent-Sys <renaldiapriyanto419@gmail.com>
2024-06-06 13:01:32 +0700

    chore: Add DishCard component for displaying dish information in MainMenu page

---
+++ src/components/MainMenu/Tab1/DishCard.tsx
@@ -0,0 +1,30 @@
+import { Dish } from "@/interfaces/IDishDaily";
+import { IonIcon } from "@ionic/react";
+import { chevronForwardCircleOutline, sunny } from "ionicons/icons";
+
+function DishCard({ name, nation, type }: Partial<Dish>) {
+  return (
+    <div className="mt-3 bg-white rounded p-4 shadow flex flex-row">
+      <img className="rounded" src="https://placehold.co/90" alt="" />
+      <div className="ml-4 w-full">
+        <div className="text-[#F4C478] flex flex-row items-center font-bold">
+          <p className="mr-1">{type}</p>
+          <div className="w-full text-xl">
+            <div className="flex flex-row items-center">
+              <IonIcon icon={sunny} />
+            </div>
+          </div>
+          <p>{nation}</p>
+        </div>
+        <div className="mt-4 font-semibold flex flex-row items-center">
+          <p className="w-full">{name}</p>
+          <div className="text-3xl flex flex-row items-center">
+            <IonIcon icon={chevronForwardCircleOutline} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DishCard;
--- src/pages/MainMenu/Tab1/index.tsx
+++ src/pages/MainMenu/Tab1/index.tsx
@@ -1,19 +1,149 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-
+import { IDishDaily } from "@/interfaces/IDishDaily";
+import { Axios } from "@/plugins/Axios";
+import {
+  IonContent,
+  IonIcon,
+  IonPage,
+  IonSpinner,
+} from "@ionic/react";
+import { useEffect, useState } from "react";
+import AnjingMelet from "@resources/img/anjing_melet.png";
+import {
+  chevronBackCircleOutline,
+  chevronForwardCircleOutline,
+  lockClosed,
+} from "ionicons/icons";
+import { getDaysInMonth, useCalender } from "@/plugins/Calender";
+import DishCard from "@/components/MainMenu/Tab1/DishCard";
 const Tab1: React.FC = () => {
+  const [dishDaily, setDishDaily] = useState<IDishDaily | null>();
+  const [premium, setPremium] = useState<boolean>(false);
+  const daysInMonth = getDaysInMonth(
+    new Date().getMonth() + 1,
+    new Date().getFullYear()
+  );
+  const { startDate, goToNextWeek, goToPreviousWeek } =
+    useCalender(daysInMonth);
+  const renderCalender = () => {
+    const calendar: JSX.Element[] = [];
+    let currentDay = startDate;
+    const today = new Date().getDate();
+    for (let i = 0; i < 7; i++) {
+      calendar.push(
+        <div key={i} className="w-full">
+          {currentDay + i <= daysInMonth ? (
+            <div className="font-bold text-center">
+              <p className="text-[#7E818C]">
+                {new Date(
+                  new Date().getFullYear(),
+                  new Date().getMonth(),
+                  currentDay + i
+                ).toLocaleString("id-ID", { weekday: "short" })}
+              </p>
+              <p
+                className={`${
+                  currentDay + i === today
+                    ? "bg-[#9FBF1F] text-white rounded-full"
+                    : ""
+                }`}
+              >
+                {currentDay + i}
+              </p>
+            </div>
+          ) : (
+            <p className="font-bold text-black text-center">-</p>
+          )}
+        </div>
+      );
+    }
+    return calendar;
+  };
+  useEffect(() => {
+    if (dishDaily == null) {
+      Axios.get("/dish/daily").then((res) => {
+        const result: IDishDaily = res.data;
+        const sortedResult: IDishDaily = {};
+        Object.keys(result)
+          .sort()
+          .forEach((key) => {
+            sortedResult[key] = result[key];
+          });
+        setDishDaily(sortedResult);
+      });
+    }
+  }, []);
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
+        <div className="ion-padding">
+          <div className="flex flex-row items-center">
+            <p className="text-4xl font-bold w-full h-min ">
+              Hi, <br />
+              <span className="text-[#F3B660]">Friend !</span>
+            </p>
+            <img src={AnjingMelet} alt="Anjing Melet" />
+          </div>
+          <div className="flex flex-row justify-evenly">
+            <p className="w-1/3 text-center font-bold py-3 border-b-[#F3B660] border-b-2">
+              Utama
+            </p>
+            {premium ? (
+              <p className="w-1/3 text-center font-bold py-3">Analisa</p>
+            ) : (
+              <p className="w-1/3 font-bold py-3 flex flex-row items-center justify-center text-[#9E9E9E]">
+                Analisa
+                <IonIcon icon={lockClosed} />
+              </p>
+            )}
+          </div>
+          <div className="mt-3">
+            <div className="flex justify-center">
+              <p className="rounded-full bg-[#774C2A] text-white font-bold px-2 py-1">
+                Kalender
+              </p>
+            </div>
+          </div>
+          <div className="mt-3">
+            <div className="flex flex-row items-center">
+              <div className="flex flex-row items-center">
+                <IonIcon
+                  icon={chevronBackCircleOutline}
+                  onClick={goToPreviousWeek}
+                  size="large"
+                />
+              </div>
+              {renderCalender()}
+              <div className="flex flex-row items-center">
+                <IonIcon
+                  icon={chevronForwardCircleOutline}
+                  onClick={goToNextWeek}
+                  size="large"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="mt-3">
+            {
+              dishDaily == null && (
+                <IonSpinner name="dots" />
+              )
+            }
+            {dishDaily &&
+              Object.keys(dishDaily).map((key) => {
+                return (
+                  <DishCard
+                    key={key}
+                    id={key}
+                    name={dishDaily[key].name}
+                    nation={dishDaily[key].nation}
+                    groceries={dishDaily[key].groceries}
+                    type={dishDaily[key].type}
+                    recipe={dishDaily[key].recipe}
+                  />
+                );
+              })}
+          </div>
+        </div>
       </IonContent>
     </IonPage>
   );
