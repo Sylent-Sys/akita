5f939c7a7dc51713cd755a38d9a5f8ad61e9822b
Sylent-Sys <renaldiapriyanto419@gmail.com>
2024-06-11 23:45:28 +0700

    chore: Add under construction message and icon to Tab3 page

--- src/pages/MainMenu/Tab3/index.tsx
+++ src/pages/MainMenu/Tab3/index.tsx
@@ -1,9 +1,18 @@
-import { IonContent, IonPage } from '@ionic/react';
+import { IonContent, IonIcon, IonPage } from "@ionic/react";
+import { construct } from "ionicons/icons";
 
 const Tab3: React.FC = () => {
   return (
     <IonPage>
       <IonContent fullscreen>
+        <div className="w-[100vw] h-[100vh] flex items-center justify-center">
+          <p className="text-2xl font-bold">
+            Under Construction{" "}
+            <span className="text-[#F3B660]">
+              <IonIcon className="text-sm" icon={construct} />
+            </span>
+          </p>
+        </div>
       </IonContent>
     </IonPage>
   );
